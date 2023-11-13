b2edf6eb150ac74de2ef50a66b897b5e94a92c5e
Kuzu <kuz14544@spengergasse.at>
2023-11-13 11:22:03 +0100

    CREATED (merged): producer storageObjectMeta StorageObject

--- src/main/java/at/spengergasse/sj2324seedproject/domain/StorageObject.java
+++ src/main/java/at/spengergasse/sj2324seedproject/domain/StorageObject.java
@@ -1,15 +1,35 @@
 package at.spengergasse.sj2324seedproject.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 
-public class StorageObject {
+@Entity
+public class StorageObject extends AbstractPersistable<Long>{
+    @Column(name = "producer")
+    private Producer          producer;
+    @Column(name = "storage_object_meta")
+    private StorageObjectMeta storageObjectMeta;
+    @Column(name = "serial_number")
+    private String            serialNumber;
+    @Column(name = "mac_address")
 
+    private String            macAddress;
+    @Column(name = "remark")
+    private String            remark;
 
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(foreignKey =@ForeignKey(name = "FK_StorageObject_2_Storage"))
+    @Column(name = "project_device")
+    private boolean projectDevice;
+    @Column(name = "status")
+    private Status  status;
+    @Column(name = "storage")
     private Storage storage;
 
-
-    private Status status;
+    @Column(name = "reservation")
+    private Reservation reservation;
+    @Column(name = "stored_at_customer")
+    private Customer    storedAtCustomer;
+    @Column(name = "stored_at_user")
+    private User        storedAtUser;
 
 }
