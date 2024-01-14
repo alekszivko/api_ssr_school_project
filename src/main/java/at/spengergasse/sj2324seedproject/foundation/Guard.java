c878f0fc8be61a08123beb307a339af4e1ebf790
Aleksandar Zivkovic <ziv22671@spengergasse.at>
2024-01-14 17:44:56 +0100

    Add Guard ensureNotNull & ensureMatchesPattern
    Tests for Guard

--- src/main/java/at/spengergasse/sj2324seedproject/foundation/Guard.java
+++ src/main/java/at/spengergasse/sj2324seedproject/foundation/Guard.java
@@ -1,15 +1,49 @@
 package at.spengergasse.sj2324seedproject.foundation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Guard {
 
-    public static <T> T isNotNull(T object, String name){
-        if (object == null){
-            throw new IllegalArgumentException("'%s' must not be null!".formatted(name));
-        }
-        return object;
+  public static <T> T isNotNull(T object, String name) {
+    if (object == null) {
+      throw new IllegalArgumentException("'%s' must not be null!".formatted(name));
+    }
+    return object;
+  }
+
+  public static <T> T isNotNull(T object) {
+    return isNotNull(object, "argument");
+  }
+
+  public static <T> T ensureNotNull(T argument) {
+    return ensureNotNull(argument, "argument");
+  }
+
+  public static <T> T ensureNotNull(T argument, String argumentName) {
+    if (argument == null) {
+      throw new IllegalArgumentException("'%s' must not be null".formatted(argumentName));
     }
 
-    public static <T> T isNotNull(T object){
-        return isNotNull(object,"argument");
+    return argument;
+  }
+
+  public static String ensureMatchesPattern(String argument, String pattern) {
+    return ensureMatchesPattern(argument, pattern, "argument");
+  }
+
+  public static String ensureMatchesPattern(String argument, String pattern, String argumentName) {
+    Pattern compiledPattern = Pattern.compile(pattern);
+    Matcher m = compiledPattern.matcher(argument);
+    if (!m.matches()) {
+      throw new IllegalArgumentException(
+          "'%s' must match pattern '%s'".formatted(argumentName, pattern));
     }
+
+    return argument;
+  }
+
+  public static String ensureMatchesBase58(String argument) {
+    return ensureMatchesPattern(argument, "[A-HJ-NP-Za-km-z1-9]*", "argument");
+  }
 }
--- src/test/java/at/spengergasse/sj2324seedproject/foundation/GuardTest.java
+++ src/test/java/at/spengergasse/sj2324seedproject/foundation/GuardTest.java
@@ -1,28 +1,72 @@
 package at.spengergasse.sj2324seedproject.foundation;
 
-import org.junit.jupiter.api.Test;
-
+import static at.spengergasse.sj2324seedproject.foundation.Guard.ensureMatchesBase58;
+import static at.spengergasse.sj2324seedproject.foundation.Guard.ensureMatchesPattern;
+import static at.spengergasse.sj2324seedproject.foundation.Guard.ensureNotNull;
 import static at.spengergasse.sj2324seedproject.foundation.Guard.isNotNull;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class GuardTest {
 
-    @Test
-    void ensureNotNullWithNonNullArgumentReturnsArgument(){
-       assertThat(isNotNull("blalba")).isEqualTo("blalba");
+  @Test
+  void ensureNotNullWithNonNullArgumentReturnsArgument() {
+    assertThat(isNotNull("blalba")).isEqualTo("blalba");
 
-    }
+  }
 
-    @Test
-    void ensureNotNullWithNullThrowsException(){
-        var exception = assertThrows(IllegalArgumentException.class, () -> isNotNull(null));
-    }
+  @Test
+  void ensureNotNullWithNullThrowsException() {
+    var exception = assertThrows(IllegalArgumentException.class, () -> isNotNull(null));
+  }
 
-    @Test
-    void ensureNotNullWithNullAndNameThrowsException(){
-        var exception = assertThrows(IllegalArgumentException.class, () -> isNotNull(null,"XXX"));
-        assertThat(exception).hasMessageContaining("'XXX' must not be null!");
-    }
+  @Test
+  void ensureNotNullWithNullAndNameThrowsException() {
+    var exception = assertThrows(IllegalArgumentException.class, () -> isNotNull(null, "XXX"));
+    assertThat(exception).hasMessageContaining("'XXX' must not be null!");
+  }
 
+  @Test
+  void ensureMatchesBase58WithInvalidArgumentThrowsException() {
+    var exception = assertThrows(IllegalArgumentException.class, () ->
+        ensureMatchesBase58("12§"));
+
+    assertThat(exception).hasMessage("'argument' must match pattern '[A-HJ-NP-Za-km-z1-9]*'");
+  }
+
+  @Test
+  void ensureMatchesBase58WithValidArgumentReturnsArgument() {
+    assertThat(ensureMatchesBase58("A")).isEqualTo("A");
+  }
+
+  @Test
+  void ensureMatchesPatternWithInvalidCharacterThrowsException() {
+    var exception = assertThrows(IllegalArgumentException.class, () ->
+        ensureMatchesPattern("0*§", "^[A-HJ-NP-Za-km-z1-9]*$"));
+
+    assertThat(exception).hasMessage("'argument' must match pattern '^[A-HJ-NP-Za-km-z1-9]*$'");
+  }
+
+  @Test
+  void ensureIsNotNullWithNonNullArgumentReturnsArgument() {
+    assertThat(ensureNotNull("B")).isEqualTo("B");
+  }
+
+  @Test
+  void ensureIsNotNullWithNullThrowsException() {
+    var exception = assertThrows(IllegalArgumentException.class, () ->
+        ensureNotNull(null));
+
+    assertThat(exception).hasMessage("'argument' must not be null");
+  }
+
+  @Test
+  void ensureIsNotNullWithNullAndNameThrowsException() {
+    var exception = assertThrows(IllegalArgumentException.class, () ->
+        ensureNotNull(null, "AAA"));
+
+    assertThat(exception).hasMessage("'AAA' must not be null");
+  }
 }
