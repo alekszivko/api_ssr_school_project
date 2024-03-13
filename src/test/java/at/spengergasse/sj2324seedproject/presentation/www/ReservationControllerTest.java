908263de609b88cbeb10b76d357d9a32e8cf1159
Aleksandar Zivkovic <ziv22671@spengergasse.at>
2024-03-13 16:16:16 +0100

    add ReservationController, fragments(grid, button), Reservation List View
    add ControllerTest
    
    Signed-off-by: Aleksandar Zivkovic <ziv22671@spengergasse.at>

---
+++ src/test/java/at/spengergasse/sj2324seedproject/presentation/www/ReservationControllerTest.java
@@ -0,0 +1,42 @@
+package at.spengergasse.sj2324seedproject.presentation.www;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import at.spengergasse.sj2324seedproject.domain.Reservation;
+import at.spengergasse.sj2324seedproject.fixture.FixtureFactory;
+import at.spengergasse.sj2324seedproject.service.ReservationService;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ReservationController.class)
+class ReservationControllerTest {
+
+  private @Autowired MockMvc mockMvc;
+  private @MockBean ReservationService reservationService;
+
+  @Test
+  void ensureGetReservationsReturnsProperView() throws Exception {
+    List<Reservation> reservations = List.of(FixtureFactory.reservationFixture(),
+        FixtureFactory.reservationFixture());
+
+    when(reservationService.fetchReservations(Optional.empty())).thenReturn(reservations);
+
+    // expect
+
+    mockMvc.perform(get(ReservationController.BASE_URL))
+        .andExpect(status().isOk())
+        .andExpect(model().attribute("reservations", reservations))
+        .andExpect(view().name("reservations/list"))
+        .andDo(print());
+  }
+}
