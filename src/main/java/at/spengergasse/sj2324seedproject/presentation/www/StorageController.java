330991c0563a144bf8022a43cda86178d3e4336a
Martin Moser <mos22280@spengergasse.at>
2024-04-09 11:14:13 +0200

    add StorageController

---
+++ src/main/java/at/spengergasse/sj2324seedproject/presentation/www/StorageController.java
@@ -0,0 +1,33 @@
+package at.spengergasse.sj2324seedproject.presentation.www;
+
+import at.spengergasse.sj2324seedproject.domain.Storage;
+import at.spengergasse.sj2324seedproject.foundation.ApiKeyGenerator;
+import at.spengergasse.sj2324seedproject.service.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+
+@Controller
+@RequestMapping(StorageController.BASE_URL)
+public class StorageController {
+
+    protected static final String BASE_URL = "/storages";
+    private final StorageService storageService;
+    private final ApiKeyGenerator apiKeyGenerator;
+
+    @GetMapping
+    public String getStorages(Model model) {
+        List<Storage> storages = storageService.fetchStorage(Optional.empty());
+        model.addAttribute("storages", storages);
+
+        return "storages/list";
+    }
+
+}
